76af3a710700055e342820049bbfc9d9774f7e6b
jerezjeff29@gmail.com <jerezjeff29@gmail.com>
2026-02-10 17:14:42 +0800

    Remove unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import PortfolioGrid from './components/portfolio/PortfolioGrid';
 import About from './components/portfolio/About';
 import ContactForm from './components/portfolio/ContactForm';
 import Footer from './components/portfolio/Footer';
-import React from 'react';
 
 export default function Home() {
   return (
